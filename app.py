07f37738624b1af069b1d7002206f3a068cd9743
jorgecif <jorgecif@gmail.com>
2020-08-28 09:15:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import streamlit as st
 #from textblob import TextBlob
 import pandas as pd
 import joblib
-#from sentimentClassifier import SentimentClassifier
-from .sentimentClassifier import *
+from classifier import *
 
 
 
