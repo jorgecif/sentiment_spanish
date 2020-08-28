15ba65a035da45665eaffd58fafa671aa03ff909
jorgecif <jorgecif@gmail.com>
2020-08-28 08:29:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #from textblob import TextBlob
 import pandas as pd
 import joblib
-from sentimentClassifier import *
+from sentimentClassifier import SentimentClassifier
 
 
 # Emoji
